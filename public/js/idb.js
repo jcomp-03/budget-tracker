636f5288ead3e8e1509ada281c3e44f832964210
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-05-12 00:01:19 -0400

    Created service-worker.js file in /public; registered service worker in index.html file.

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,10 +27,9 @@ request.onupgradeneeded = function (event) {
 request.onsuccess = function (event) {
   // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
   db = event.target.result;
-  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  // check if app is online, if yes run uploadTransactions() function to send all local db data to api
   if (navigator.onLine) {
-    // we haven't created this yet, but we will soon, so let's comment it out for now
-    // uploadPizza();
+    uploadTransactions();
   }
 };
 
@@ -38,7 +37,6 @@ request.onerror = function (event) {
   // log error here
   console.log(event.target.errorCode);
 };
-
 
 // This function will be executed if we attempt to submit a transaction and there's no internet connection
 function saveRecord(record) {
@@ -51,3 +49,48 @@ function saveRecord(record) {
   // add record to your store with add method
   budgetObjectStore.add(record);
 }
+
+
+function uploadTransactions() {
+  // open a transaction on your db
+  const transaction = db.transaction(["new_budget_record"], "readwrite");
+  // access your object store
+  const budgetObjectStore = transaction.objectStore("new_budget_record");
+  // get all records from store and set to a variable
+  const getAll = budgetObjectStore.getAll();
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transactions", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_budget_record"], "readwrite");
+          // access the new_budget_record object store
+          const budgetObjectStore = transaction.objectStore("new_budget_record");
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert("All saved transactions has been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransactions)
